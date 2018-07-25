c4c2c3b8123164c01437fe302d7a63d6005929fa
callahrs <callahrs@rose-hulman.edu>
2018-07-25 13:22:38 -0400

    Progress save

--- src/m6_loops_within_loops_printing.py
+++ src/m6_loops_within_loops_printing.py
@@ -5,8 +5,9 @@ This problem provides practice at:
   ***  LOOPS WITHIN LOOPS in PRINTING-TO-CONSOLE problems.  ***
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Riley Callahan.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
+
 
 ########################################################################
 # Students:
@@ -75,7 +76,7 @@ def shape(r):
     For purposes of "lining up", assume r is a single digit.
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # Done: 2. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     ####################################################################
@@ -87,6 +88,17 @@ def shape(r):
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:  15 minutes.
     # ------------------------------------------------------------------
+    for k in range(r):
+        for j in range(r - (r - k)):
+            print(' ', end='')
+        for j in range(r - k):
+            print('+', end='')
+        print('!', end='')
+        for j in range(r - k):
+            print((r - k) - j, end='')
+        for j in range(r - (r - k)):
+            print('-', end='')
+        print('')
 
 
 # ----------------------------------------------------------------------
